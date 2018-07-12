109a1aaa991e9249aebc57fd8b8e290b8e2227a7
joanissedan@gmail.com <joanissedan@gmail.com>
2018-07-12 20:30:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a+b;
+}
+
+function subtract(a, b) {
+  return a-b;
+}
+
+function multiply(a, b) {
+  return a*b;
+}
+
+function divide(a, b) {
+  return a/b;
+}
+
+function inc(n) {
+var n;
+n++
+return n;
+}
+
+function dec(n) {
+  var n;
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+  parseInt(n);
+  return n;
+}
+
+function preserveDecimal(n) {
+  parseFloat(n);
+  return n;
+}
